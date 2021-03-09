a3fc9cf6323e5630e08bf6c7b31084107dedd7fe
mschauer <moritzschauer@web.de>
2021-03-10 00:15:28 +0100

    Fix example

--- example/timelift.jl
+++ example/timelift.jl
@@ -1,3 +1,4 @@
+using DynamicIterators
 struct Squares
 end
 Base.iterate(S::Squares, state=1) = (state*state, state+1)
@@ -7,5 +8,5 @@ using DynamicIterators
 T = TimeLift(Squares())
 # Create NextKey control slot
 C = attach(NextKeys, T)
-# Bind iterator 4:2:8 to the control slot and collect
-collect(bind(4:2:8, C))
+# Bind iterator [1;4:2:8] to the control slot and collect
+collect(bind([1; 4:2:8], C))
